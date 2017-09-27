9a3d112ed832e4eb76fd75de1e78a132e106d433
yuxixixi <yuxixixi1995@163.com>
2017-09-27 15:53:42 +0800

    weather03

---
+++ app/src/main/java/cn/edu/pku/yuxi/miniweather/MainActivity.java
@@ -0,0 +1,18 @@
+package cn.edu.pku.yuxi.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yuxi on 17/9/27.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+
+}
